2a32e7caf876b92ad8aff087b7b60b1f2216ba4f
Gingernan <ginger_nan@163.com>
2025-11-12 19:32:53 +0800

    添加一个Acceptor类

---
+++ code/day07-为我们的服务器添加一个Acceptor/src/Acceptor.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <functional>
+
+class EventLoop;
+class Socket;
+class InetAddress;
+class Channel;
+class Acceptor
+{
+public:
+    Acceptor(EventLoop* loop);
+    ~Acceptor();
+
+    void acceptConnection();
+
+    void setNewConnectionCallback(std::function<void(Socket*)>);
+private:
+    EventLoop* m_loop;
+    Socket* m_sock;
+    InetAddress* m_addr;
+    Channel* m_acceptChannel;
+    std::function<void(Socket*)> m_newConnectionCallback;
+};
---
+++ code/day07-为我们的服务器添加一个Acceptor/src/Server.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class EventLoop;
+class Socket;
+class Acceptor;
+
+class Server
+{
+public:
+    Server(EventLoop* loop);
+    ~Server();
+
+    // 读事件
+    void handleReadEvent(int);
+
+    // 新建连接事件
+    void newConnection(Socket* serv_sock);
+private:
+    EventLoop* m_loop;
+    Acceptor* m_acceptor;
+};
